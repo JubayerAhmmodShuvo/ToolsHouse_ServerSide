cc743353a2d1ad7dd515c9da65805f65f68b46e2
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-05-22 19:10:18 +0600

    users are listed in a collection using jwt

--- index.js
+++ index.js
@@ -19,12 +19,51 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ error: "Unauthorized Access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ error: "Forbidden" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 async function run() { 
   try {
     await client.connect();
     console.log("Connected correctly to server");
-    const usersCollection = client.db("squirrel-manufacturer").collection("users");
+      const usersCollection = client
+        .db("squirrel-manufacturer")
+        .collection("users");
     
+
+      app.get("/user", verifyJWT, async (req, res) => {
+        const users = await usersCollection.find().toArray();
+        res.send(users);
+      });
+      app.put("/user/:email", async (req, res) => {
+        const emial = req.params.email;
+        const user = req.body;
+        const filter = { email: emial };
+        const options = { upsert: true };
+        const updateDoc = {
+          $set: user,
+        };
+        const result = await usersCollection.updateOne(
+          filter,
+          updateDoc,
+          options
+        );
+        const token = jwt.sign({ email: emial }, process.env.ACCESS_TOKEN, {
+          expiresIn: "1h",
+        });
+        res.send({ result, token: token });
+      });
     
   }
   finally { }
