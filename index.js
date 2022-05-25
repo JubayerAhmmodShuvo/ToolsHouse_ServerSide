8191edb8613c1ffa1d8c44921e6ef652615c7893
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-05-26 00:18:09 +0600

    make admin jwt done

--- index.js
+++ index.js
@@ -54,6 +54,18 @@ async function run() {
         .db("squirrel-manufacturer")
         .collection("payment");
 
+     const verifyAdmin = async (req, res, next) => {
+       const requester = req.decoded.email;
+       const requesterAccount = await usersCollection.findOne({
+         email: requester,
+       });
+       if (requesterAccount.role === "admin") {
+         next();
+       } else {
+         res.status(403).send({ message: "forbidden" });
+       }
+     };
+
     app.post("/order", async (req, res) => { 
       const order = req.body;
       const result = await orderCollection.insertOne(order);
@@ -151,8 +163,15 @@ async function run() {
         });
         res.send({ result, token: token });
       });
+
+     app.get('/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = await usersCollection.findOne({ email: email });
+      const isAdmin = user.role === 'admin';
+      res.send({ admin: isAdmin })
+    })
     
-     app.put("/user/admin/:email",  async (req, res) => {
+     app.put("/user/admin/:email",verifyJWT,verifyAdmin,  async (req, res) => {
        const email = req.params.email;
        const filter = { email: email };
        const updateDoc = {
