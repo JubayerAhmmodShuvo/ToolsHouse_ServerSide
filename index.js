e1dd27d52a0f46ccd946ceca1b55563c7539b2fc
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2023-11-29 00:23:20 +0600

    error

--- index.js
+++ index.js
@@ -11,10 +11,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(json());
 
-
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xt1bf.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
