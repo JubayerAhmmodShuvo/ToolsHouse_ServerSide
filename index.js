0a3671386a374f9a388aaf45158fc08ed29f002c
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2023-11-28 12:14:52 +0600

    error solving

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ app.use(express.json());
 
 
 
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xt1bf.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
